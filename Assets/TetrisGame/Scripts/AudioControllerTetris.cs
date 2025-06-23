93d3601a27dcb5814b62cdc95de789c32a6e41f5
AndresPedroza-Games <andresepedrozaruni@gmail.com>
2025-06-23 13:00:14 +0200

    Add few things

---
+++ Assets/TetrisGame/Scripts/AudioControllerTetris.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AudioControllerTetris : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
+    public AudioClip levelUp;
+    public AudioClip gameOver;
+    public AudioClip selectPiece;
+    public AudioClip loseHeart;
+
+    public void MakeSound(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+}
